e68f21e546305c69f4cbdaf50e4c663f72d66cb0
teng <teng2002823@outlook.com>
2022-09-18 01:42:12 +0800

    constant

---
+++ src/main/java/com/lt/learningredis/constant/RedisConstants.java
@@ -0,0 +1,21 @@
+package com.lt.learningredis.constant;
+
+/**
+ * @author teng
+ */
+public class RedisConstants {
+    public static final String LOGIN_CODE_KEY = "login:code:";
+    public static final Long LOGIN_CODE_TTL = 2L;
+    public static final String LOGIN_USER_KEY = "login:token:";
+    public static final Long LOGIN_USER_TTL = 30L;
+
+    public static final Long CACHE_NULL_TTL = 2L;
+
+    public static final Long CACHE_SHOP_TTL = 30L;
+    public static final String CACHE_SHOP_KEY = "cache:shop:";
+
+    public static final String LOCK_SHOP_KEY = "lock:shop:";
+    public static final Long LOCK_SHOP_TTL = 10L;
+
+    public static final String SECKILL_STOCK_KEY = "seckill:stock:";
+}
---
+++ src/main/java/com/lt/learningredis/constant/SystemConstants.java
@@ -0,0 +1,15 @@
+package com.lt.learningredis.constant;
+
+/**
+ * @author teng
+ */
+public interface SystemConstants {
+    String IMAGE_UPLOAD_DIR = "C:\\develop\\idea-space\\hm-dianping-web\\imgs\\";
+    String USER_NICK_NAME_PREFIX = "user_";
+    int DEFAULT_PAGE_SIZE = 5;
+    int MAX_PAGE_SIZE = 10;
+
+    String USER_SESSION_CODE = "code";
+
+    String USER_SESSION_USER = "user";
+}
