c32b85e3551ab49cffa0698da817701da2697474
teng <teng2002823@outlook.com>
2022-09-18 01:42:21 +0800

    controller

---
+++ src/main/java/com/lt/learningredis/controller/UserController.java
@@ -0,0 +1,86 @@
+package com.lt.learningredis.controller;
+
+
+import com.lt.learningredis.dto.LoginFormDTO;
+import com.lt.learningredis.dto.Result;
+import com.lt.learningredis.dto.UserDTO;
+import com.lt.learningredis.entity.UserInfo;
+import com.lt.learningredis.service.IUserInfoService;
+import com.lt.learningredis.service.IUserService;
+import com.lt.learningredis.utils.UserHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 前端控制器
+ *
+ * @author teng
+ */
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private IUserService userService;
+
+    @Resource
+    private IUserInfoService userInfoService;
+
+    /**
+     * 发送手机验证码
+     */
+    @PostMapping("/code")
+    public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
+        return userService.sendCode(phone, session);
+    }
+
+    /**
+     * 登录功能
+     *
+     * @param loginForm 登录参数，包含手机号、验证码；或者手机号、密码
+     */
+    @PostMapping("/login")
+    public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session) {
+        // 实现登录功能
+        return userService.login(loginForm, session);
+    }
+
+    /**
+     * 登出功能
+     *
+     * @return 无
+     */
+    @PostMapping("/logout")
+    public Result logout() {
+        // TODO 实现登出功能
+        return Result.fail("功能未完成");
+    }
+
+    /**
+     * 获取当前用户
+     */
+    @GetMapping("/me")
+    public Result me() {
+        // 获取当前登录的用户并返回
+        UserDTO user = UserHolder.getUser();
+        return Result.ok(user);
+    }
+
+    @GetMapping("/info/{id}")
+    public Result info(@PathVariable("id") Long userId) {
+        // 查询详情
+        UserInfo info = userInfoService.getById(userId);
+        if (info == null) {
+            // 没有详情，应该是第一次查看详情
+            return Result.ok();
+        }
+        info.setCreateTime(null);
+        info.setUpdateTime(null);
+        // 返回
+        return Result.ok(info);
+    }
+}
---
+++ src/main/java/com/lt/learningredis/controller/VoucherOrderController.java
@@ -0,0 +1,25 @@
+package com.lt.learningredis.controller;
+
+
+import com.lt.learningredis.dto.Result;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@RestController
+@RequestMapping("/voucher-order")
+public class VoucherOrderController {
+    @PostMapping("seckill/{id}")
+    public Result seckillVoucher(@PathVariable("id") Long voucherId) {
+        return Result.fail("功能未完成");
+    }
+}
