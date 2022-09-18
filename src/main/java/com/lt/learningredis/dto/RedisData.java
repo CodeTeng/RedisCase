3530d02b8a66b896b574b22cc42c6a70fc48f180
teng <teng2002823@outlook.com>
2022-09-18 22:39:30 +0800

    dto

--- src/main/java/com/lt/learningredis/dto/RedisData.java
+++ src/main/java/com/lt/learningredis/dto/RedisData.java
@@ -1,11 +1,17 @@
-package com.lt.learningredis.utils;
+package com.lt.learningredis.dto;
 
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * @author teng
+ */
 @Data
 public class RedisData {
+    /**
+     * 逻辑过期时间
+     */
     private LocalDateTime expireTime;
     private Object data;
 }
