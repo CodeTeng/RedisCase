ae9595f6ade7607bc79cb508a7998894b386666c
teng <teng2002823@outlook.com>
2022-09-18 22:39:57 +0800

    init

--- src/main/java/com/lt/learningredis/LearningredisApplication.java
+++ src/main/java/com/lt/learningredis/LearningredisApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author teng
+ */
 @SpringBootApplication
 @MapperScan("com.lt.learningredis.mapper")
 public class LearningredisApplication {
