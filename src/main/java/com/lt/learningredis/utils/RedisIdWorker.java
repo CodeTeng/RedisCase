61e19264ae5475da501ac96184e4dac8bee86721
teng <teng2002823@outlook.com>
2022-09-21 00:24:02 +0800

    utils

--- src/main/java/com/lt/learningredis/utils/RedisIdWorker.java
+++ src/main/java/com/lt/learningredis/utils/RedisIdWorker.java
@@ -3,40 +3,52 @@ package com.lt.learningredis.utils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * @author teng
+ */
 @Component
 public class RedisIdWorker {
     /**
-     * 开始时间戳
+     * 开始时间戳-2022-1-1-0-0-0
      */
     private static final long BEGIN_TIMESTAMP = 1640995200L;
+
     /**
      * 序列号的位数
      */
     private static final int COUNT_BITS = 32;
 
+    @Resource
     private StringRedisTemplate stringRedisTemplate;
 
-    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
-        this.stringRedisTemplate = stringRedisTemplate;
-    }
-
+    /**
+     * 生成全局唯一ID
+     *
+     * @param keyPrefix 业务前缀
+     * @return 全局唯一ID
+     */
     public long nextId(String keyPrefix) {
-        // 1.生成时间戳
+        // 1. 生成时间戳
         LocalDateTime now = LocalDateTime.now();
         long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
         long timestamp = nowSecond - BEGIN_TIMESTAMP;
-
-        // 2.生成序列号
-        // 2.1.获取当前日期，精确到天
+        // 2. 生成序列号
+        // 2.1. 获取当前日期，精确到天
         String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
-        // 2.2.自增长
+        // 2.2. 自增长
         long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
-
-        // 3.拼接并返回
+        // 3. 拼接并返回
         return timestamp << COUNT_BITS | count;
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime time = LocalDateTime.of(2022, 1, 1, 0, 0, 0);
+        long second = time.toEpochSecond(ZoneOffset.UTC);
+        System.out.println("second = " + second);
     }
 }
