122e3613f81f6d53960598888a5290e5dd9b0131
teng <teng2002823@outlook.com>
2022-09-21 00:24:08 +0800

    init

--- src/main/java/com/lt/learningredis/LearningredisApplication.java
+++ src/main/java/com/lt/learningredis/LearningredisApplication.java
@@ -3,12 +3,14 @@ package com.lt.learningredis;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author teng
  */
 @SpringBootApplication
 @MapperScan("com.lt.learningredis.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class LearningredisApplication {
 
     public static void main(String[] args) {
