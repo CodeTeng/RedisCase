3063b7093671cf79cbe7ad3df39bf38b2b5e50c9
teng <teng2002823@outlook.com>
2022-09-21 00:23:14 +0800

    mapper

--- src/main/java/com/lt/learningredis/mapper/SeckillVoucherMapper.java
+++ src/main/java/com/lt/learningredis/mapper/SeckillVoucherMapper.java
@@ -2,15 +2,15 @@ package com.lt.learningredis.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lt.learningredis.entity.SeckillVoucher;
+import org.springframework.stereotype.Repository;
+
 
 /**
- * <p>
  * 秒杀优惠券表，与优惠券是一对一关系 Mapper 接口
- * </p>
  *
- * @author 虎哥
- * @since 2022-01-04
+ * @author teng
  */
+@Repository
 public interface SeckillVoucherMapper extends BaseMapper<SeckillVoucher> {
 
 }
--- src/main/java/com/lt/learningredis/mapper/VoucherMapper.java
+++ src/main/java/com/lt/learningredis/mapper/VoucherMapper.java
@@ -3,17 +3,15 @@ package com.lt.learningredis.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lt.learningredis.entity.Voucher;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
 /**
- * <p>
- *  Mapper 接口
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
+ * @author teng
  */
+@Repository
 public interface VoucherMapper extends BaseMapper<Voucher> {
 
     List<Voucher> queryVoucherOfShop(@Param("shopId") Long shopId);
